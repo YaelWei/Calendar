2d132694d06ed1144238456ccd6f92dfd6f48060
YaelWei <55844382+YaelWei@users.noreply.github.com>
2019-11-09 22:21:15 -0500

    Calendar days have numbers (used moment.js), fixed page set up for eventual sidebar

--- seCalendar/Calendar/script.js
+++ seCalendar/Calendar/script.js
@@ -4,6 +4,7 @@ var navMonth = document.querySelector('.calendar-nav-date');
 const now =  moment();
 //sets up initial page
 navMonth.textContent = now.format('MMMM YYYY');
+populateCalendar(now);
 
 function addMonth(e) {
   now.add(1, 'month');
@@ -21,16 +22,19 @@ navLeftButton.addEventListener("click",(e) =>{
   //submit has a default action (refresh), preventDefault doesn't allow that behavior to occur
   e.preventDefault();
   subtractMonth(e);
+  populateCalendar(now);
+
   
 })
 navRightButton.addEventListener("click",(e) =>{
   //submit has a default action (refresh), preventDefault doesn't allow that behavior to occur
   e.preventDefault();
   addMonth(e);
+  populateCalendar(now);
   
 })
-function populateCalendar() {
-  const now = moment();
+function populateCalendar(date) {
+  
   const days = Array.from(document.querySelectorAll('.day'));
   const dayIndices = new Map([
     ['Monday', 0],
@@ -41,19 +45,22 @@ function populateCalendar() {
     ['Saturday', 5],
     ['Sunday', 6]
   ]);
-  const firstDayOfMonth = moment().date(1);
+  //can't mutate date, so clone it.
+  const firstDayOfMonth = date.clone().date(1);
+  //format is not a mutating method
   console.log(firstDayOfMonth.format())
   // Once you have the first day of the month, you need to know the index.
   // The dictionary keys are day names. I get the day of the first month like 'Friday' to match
   // an index of 4.
   const firstDayIndex = dayIndices.get(firstDayOfMonth.format('dddd'));
-  const firstDayOfCalendar = moment(firstDayOfMonth).subtract(firstDayIndex, 'days');
-  let iterator = moment(firstDayOfCalendar);
+  const dayOfFirstBox = firstDayOfMonth.clone().subtract(firstDayIndex, 'days');
+  let iterator = dayOfFirstBox.clone()
 
   for (let i = 0; i < days.length; i++) {
     const dayDiv = days[i];
     dayDiv.textContent = iterator.date();
     iterator = iterator.add(1, 'days');
   }
-  console.log(firstDayOfCalendar.format('dddd, MM-DD-YYYY'));
+  console.log(dayOfFirstBox.format('dddd, MM-DD-YYYY'));
 }
+
