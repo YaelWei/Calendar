e0d417e5fc35debdfd5981986fd2548bfd9bcc84
Frankie Colasurdo <frankiecolasurdo@mail.adelphi.edu>
2019-11-20 10:49:10 -0500

    Added get and insert files for frontend

---
+++ seCalendar/Calendar/getEvents.php
@@ -0,0 +1,29 @@
+<?php
+
+//test.php
+
+$connect = new PDO('mysql:host=localhost;dbname=f19seaucalendar', 'frankcolasurdo', 'frcolsefp');
+
+$data = array();
+
+$query = "SELECT * FROM events";
+
+$statement = $connect->prepare($query);
+
+$statement->execute();
+
+$result = $statement->fetchAll();
+
+foreach($result as $row)
+{
+ $data[] = array(
+  'id'   => $row["eventID"],
+  'title'   => $row["eventName"],
+  'description'   => $row["eventDescription"],
+  'date'   => $row["eventDate"],
+ );
+}
+
+echo json_encode($data);
+
+?>
