a0fa03af203e837f567c5c192a23eed2407ad5a4
YaelWei <55844382+YaelWei@users.noreply.github.com>
2019-11-24 18:13:12 -0500

    add pseudoevents to calendar

--- seCalendar/Calendar/script.js
+++ seCalendar/Calendar/script.js
@@ -5,6 +5,7 @@ const now = moment();
 //sets up initial page
 navMonth.textContent = now.format('MMMM YYYY');
 populateCalendar(now);
+getEvents();
 
 function addMonth(e) {
   now.add(1, 'month');
@@ -23,6 +24,7 @@ navLeftButton.addEventListener("click", (e) => {
   e.preventDefault();
   subtractMonth(e);
   populateCalendar(now);
+  getEvents();
 
 
 })
@@ -31,6 +33,7 @@ navRightButton.addEventListener("click", (e) => {
   e.preventDefault();
   addMonth(e);
   populateCalendar(now);
+  getEvents();
 
 })
 function populateCalendar(date) {
@@ -58,7 +61,11 @@ function populateCalendar(date) {
 
   for (let i = 0; i < days.length; i++) {
     const dayDiv = days[i];
+    dayDiv.innerHTML = '';
     dayDiv.textContent = iterator.date();
+    dayDiv.dataset.day = iterator.date();
+    dayDiv.dataset.month = iterator.month();
+    dayDiv.dataset.year = iterator.year();
     iterator = iterator.add(1, 'days');
   }
   console.log(dayOfFirstBox.format('dddd, MM-DD-YYYY'));
@@ -93,3 +100,113 @@ nav.addEventListener("click", (e) => {
   }
 })
 
+async function getEvents() {
+  // const response = await fetch('url')
+  // let events = await response.json();
+  let events = `
+  [
+    { "title": "event1", "date": "2019-11-20"},
+    { "title": "event2", "date": "2019-12-08"},
+    { "title": "Yael's Birthday", "date": "2019-11-22"},
+    { "title": "Yael's Birthday", "date": "2019-11-22"},
+    { "title": "Yael's Birthday", "date": "2019-11-22"},
+    { "title": "Yael's Super Cool Birthday", "date": "2019-11-22"},
+    { "title": "event4", "date": "2019-11-23"},
+    { "title": "event5", "date": "2019-10-28"},
+    { "title": "event6", "date": "2019-09-21"},
+    { "title": "event7", "date": "2019-11-22"},
+    { "title": "event8", "date": "2019-10-23"}
+
+  ]
+  `;
+
+  events = JSON.parse(events);
+   // sort the events
+   events.sort((eventA, eventB) => {
+    var a = moment(eventA.date);
+    var b = moment(eventB.date);
+    return a.diff(b);
+   })
+   console.log(events);
+   var days = document.querySelectorAll(".day");
+  //  var firstDay, lastDay;
+  //  if (days) {
+  //    firstDay = parseInt(days[0].textContent, 10);
+  //    lastDay = parseInt(days[days.length - 1].textContent, 10);
+
+  //    console.log(firstDay, lastDay);
+  //  }
+  //  if (firstDay > 1 ) {
+  //   //if the first day is greater than one, then that date is part of the previous month
+  //   var previousMonth = moment().subtract(1, 'months');
+  //   previousMonth.date(firstDay);
+  //   firstDay = previousMonth;
+  //   console.log('first day: ' , firstDay);
+  //  } else {
+  //    //the first day is part of the current month
+  //    firstDay = moment().date(1);
+  //  }
+  //  console.log(moment().endOf("month").date())
+  //  var currentMonthsLastDate = moment().endOf("month").date()
+  //  if (lastDay < currentMonthsLastDate) {
+  //    //if the last date in the whole calendar is less then the current Month's last date, then it is part of the next month
+  //    var nextMonth = moment().add(1, 'months');
+  //    nextMonth.date(lastDay);
+  //    lastDay = nextMonth;
+  //  } else {
+  //    lastDay = currentMonthsLastDate;
+  //  }
+
+   // The values are in milliseconds since Unix Epoch
+  //  const valueOfFirstDay = firstDay.set({hour:0,minute:0,second:0,millisecond:0}).valueOf();
+  //  const valueOfLastDay = lastDay.set({hour:0,minute:0,second:0,millisecond:0}).valueOf();
+  //  console.log('first day: ', firstDay, 'last day: ', lastDay);
+
+
+
+
+
+   
+    // find the event that has the first date in your calendar month view
+    // find the event that has the last date in your calendadr month view
+    // grab all the events between those two dates
+    // for each event object
+      // place event info in correct calendar day box
+    
+
+  // const eventsToShow = []
+  // for (let event of events) {
+  //   const eventValue = moment(event.date).set({hour:0,minute:0,second:0,millisecond:0}).valueOf();
+  //   if (eventValue >= valueOfFirstDay && eventValue <= valueOfLastDay) {
+  //     eventsToShow.push(event);
+  //   }
+  // }
+
+
+
+  for (let i = 0; i < days.length; i++) {
+    const dayDiv = days[i];
+    const dayDivDate = makeDateFromDayDiv(dayDiv);
+    for (let j = 0; j < events.length; j++) {
+      const event = events[j];
+      const eventMoment = moment(event.date);
+      if (dayDivDate === eventMoment.format('YYYY-MM-DD')) {
+        dayDiv.appendChild(createEventDiv(event));
+      }
+    }
+  }
+}
+
+function createEventDiv(event) {
+  const div = document.createElement('div');
+  div.classList.add('event');
+  div.textContent += event.title;
+  return div;
+}
+
+function makeDateFromDayDiv(dayDiv) {
+  var day = dayDiv.dataset.day;
+  var month = parseInt(dayDiv.dataset.month, 10) + 1;
+  var year = dayDiv.dataset.year;
+  return `${year}-${month}-${day}`; // YYYY-M-D 2019-1-1 or 2019-11-25
+}
