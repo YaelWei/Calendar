fcc34d4950e020542f92ad748005e0dde52c535a
Frankie Colasurdo <frankiecolasurdo@mail.adelphi.edu>
2019-12-05 10:04:28 -0500

    Removed backend testing directory

--- backend/backend.md
+++
@@ -1,2 +0,0 @@
-# Backend files
-SQL scripts and other backend files can be found here.
--- backend/sql_files/insert.sql
+++
@@ -1,8 +0,0 @@
-use test_calendar;
-
-INSERT INTO events VALUES
-(1, "Matriculation Day", '2019-08-25' ),
-(2, "First Day of Classes", '2019-08-26' ),
-(3, "Labor Day - No Classes", '2019-09-02' ),
-(4, "Last Day to Add a Course", '2019-09-09' ),
-(5, "Last day to Drop a Course", '2019-09-23' );
