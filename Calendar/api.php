763b357d6a098bb682bc69004c75373021c4a896
Frankie Colasurdo <frankiecolasurdo@mail.adelphi.edu>
2019-12-12 13:17:56 -0500

    api.php commented

--- Calendar/api.php
+++ Calendar/api.php
@@ -1,5 +1,7 @@
 <?php
-  // Connect to database
+ 	// api.php: Our REST API for Alexa team to utilize with GET, POST, and DELETE methods
+  
+  	// Connect to database
 	$db=mysqli_connect('localhost','cameronbosch','alliance','f19seaucalendar');
 
 	if ($db->connect_errno) {
@@ -7,6 +9,7 @@
 	exit();
 }
 	
+	// Switch cases for different REST functions
 	$request_method=$_SERVER["REQUEST_METHOD"];
 	switch($request_method)
 	{
@@ -23,20 +26,21 @@
 			}
 			break;
 		case 'POST':
-			// Insert event
+			// Insert an event
 			insert_event();
 			break;
 		case 'DELETE':
-			// Delete event
+			// Delete an event
 			$eventID=intval($_GET["eventID"]);
 			delete_event($eventID);
 			break;
 		default:
-			// Invalid Request Method
+			// Instance of an Invalid Request Method
 			header("HTTP/1.0 405 Method Not Allowed");
 			break;
 	}
 
+	// POST function
 	function insert_event()
 	{
 		global $db;
@@ -48,7 +52,7 @@
 		{
 			$response=array(
 				'status' => 1,
-				'status_message' =>'Event Added Successfully.'
+				'status_message' =>'Event Addition Successful.'
 			);
 		}
 		else
@@ -61,6 +65,8 @@
 		header('Content-Type: application/json');
 		echo json_encode($response);
 	}
+	
+	// GET function
 	function get_events($eventID=0)
 	{
 		global $db;
@@ -78,6 +84,8 @@
 		header('Content-Type: application/json');
 		echo json_encode($response);
 	}
+
+	// DELETE function
 	function delete_event($eventID)
 	{
 		global $db;
@@ -86,14 +94,14 @@
 		{
 			$response=array(
 				'status' => 1,
-				'status_message' =>'event Deleted Successfully.'
+				'status_message' =>'Event Deletion Successful.'
 			);
 		}
 		else
 		{
 			$response=array(
 				'status' => 0,
-				'status_message' =>'event Deletion Failed.'
+				'status_message' =>'Event Deletion Failed.'
 			);
 		}
 		header('Content-Type: application/json');
